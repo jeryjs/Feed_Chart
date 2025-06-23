8050dee8093850940eadd1b9281791e8fa98c9c7
Jery <jery99961@gmail.com>
2025-06-23 21:52:22 +0530

    Bump version to v2.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jery.feedchart"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "2.1.5"
+        versionCode = 10
+        versionName = "2.1.6"
     }
 
     buildTypes {
